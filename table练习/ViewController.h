db318bd18ae71300815f4bda3834f0ad4fda5091
zhangweiwei2830 <284255853@qq.com>
2016-09-06 09:45:07 +0800

    Initial Commit

---
+++ table练习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  table练习
+//
+//  Created by 牛帮主 on 16/9/6.
+//  Copyright © 2016年 niubangzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ table练习/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  table练习
+//
+//  Created by 牛帮主 on 16/9/6.
+//  Copyright © 2016年 niubangzhu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
